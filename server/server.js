5837be9967526ac7ad931d961c1d6460130b3a8b
serclino <sercl.adam@email.cz>
2022-08-12 12:57:44 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const upload = require("express-fileupload");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const app = express();
+
+let csvData = [];
+
+app.use(upload());
+
+// following code checks if there is already an uploaded file
+app.get("/data", (req, res) => {
+  csvData = [];
+  if (fs.existsSync("./uploads/file.csv")) {
+    console.log("file exists");
+    const file = __dirname + "/uploads/file.csv";
+    console.log(file);
+    fs.createReadStream(file)
+      .pipe(
+        parse({
+          delimiter: ",",
+        })
+      )
+      .on("data", (dataRow) => {
+        csvData.push(dataRow);
+      })
+      .on("end", () => {
+        console.log(csvData);
+        res.send(csvData);
+      });
+  } else {
+    console.log("file does not exist");
+  }
+});
+
+// save file to 'uploads' directory:
+app.post("/data", (req, res) => {
+  // console.log(req);
+  if (req.files) {
+    csvData = [];
+    const file = req.files.csv;
+    const filename = file.name;
+    file.mv(`./uploads/${filename}`, (err) => {
+      if (err) {
+        res.send(err); // later make this better
+      } else {
+        fs.createReadStream(__dirname + `/uploads/${filename}`)
+          .pipe(
+            parse({
+              delimiter: ",",
+            })
+          )
+          .on("data", (dataRow) => {
+            csvData.push(dataRow);
+          })
+          .on("end", () => {
+            console.log(csvData);
+            res.send(csvData);
+          });
+      }
+    });
+  }
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
