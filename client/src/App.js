2cb243b56e0bcb41de64e6c74be3d0fb8572a805
serclino <sercl.adam@email.cz>
2022-08-13 09:47:01 +0200

    successfully counting

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 const App = () => {
   const [backendData, setBackendData] = useState([]);
   const [file, setFile] = useState(null);
+  const [totalHours, setTotalHours] = useState(0);
+  const [wagePerHour, setWagePerHour] = useState(0);
 
   useEffect(() => {
     Axios({
@@ -26,7 +28,6 @@ const App = () => {
     // console.log(file);
     let formdata = new FormData();
     formdata.append("csv", file, "file.csv");
-
     Axios({
       url: "/data",
       method: "POST",
@@ -47,82 +48,107 @@ const App = () => {
       lastInput.type = "hidden";
       lastInput.value = "";
     }
-    // add input hours to totalHours
+    // submit form of the current row
+    document.getElementById(`${uniqueId}`).requestSubmit();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkedInputs = Array.from(
+      document.querySelectorAll("input[type=radio]:checked")
+    );
+    const counterArr = checkedInputs.map((input) => Number(input.value));
+    let total = 0;
+    for (let i = 0; i < counterArr.length; i++) {
+      total += counterArr[i];
+    }
+    setTotalHours(total);
   };
 
   return (
     <div>
       <h1>CSV File Upload</h1>
       <form onSubmit={(e) => handleUpload(e)}>
-        <p>Nahrání nového souboru způsobí, že se přemažou data.</p>
+        <p>Nahrání nového souboru způsobí, že se přepíšou data.</p>
         <label>Select File</label>
         <input type="file" name="file" onChange={(e) => handleFile(e)} />
         <button type="submit">Upload</button>
       </form>
 
       {backendData.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              {backendData[0]?.map((header, id) => {
-                return <th key={id}>{header}</th>;
-              })}
-              <th>Jiné</th>
-            </tr>
-          </thead>
-          <tbody>
-            {backendData.map((row, id) => {
-              if (id === 0) {
-                return null;
-              }
-              const uniqueId = `${id}-row`;
-              return (
-                <tr key={id}>
-                  {row.map((cell, id) => {
-                    if (id === 0) {
-                      return (
-                        <td key={id}>
-                          <form id={uniqueId}>{cell}</form>
-                        </td>
-                      );
-                    } else {
-                      return (
-                        <td key={id}>
-                          <input
-                            type="radio"
-                            form={uniqueId}
-                            value={cell}
-                            name={uniqueId}
-                            onClick={(e) => handleCheck(e, uniqueId)}
-                            disabled={!cell}
-                          />
-                          <label htmlFor={cell}>{cell}</label>
-                        </td>
-                      );
-                    }
-                  })}
-                  <td>
-                    <input
-                      type="radio"
-                      form={uniqueId}
-                      name={uniqueId}
-                      value=""
-                      onClick={(e) =>
-                        (document.getElementById(`lastInput-${uniqueId}`).type =
-                          "number")
+        <>
+          <table>
+            <thead>
+              <tr>
+                {backendData[0]?.map((header, id) => {
+                  return <th key={id}>{header}</th>;
+                })}
+                <th>Jiné</th>
+              </tr>
+            </thead>
+            <tbody>
+              {backendData.map((row, id) => {
+                if (id === 0) {
+                  return null;
+                }
+                const uniqueId = `${id}-row`;
+                return (
+                  <tr key={id}>
+                    {row.map((cell, id) => {
+                      if (id === 0) {
+                        return (
+                          <td key={id}>
+                            <form id={uniqueId} onSubmit={handleSubmit}>
+                              {cell}
+                            </form>
+                          </td>
+                        );
+                      } else {
+                        return (
+                          <td key={id}>
+                            <input
+                              type="radio"
+                              form={uniqueId}
+                              value={cell}
+                              name={uniqueId}
+                              onClick={(e) => handleCheck(e, uniqueId)}
+                              disabled={!cell}
+                            />
+                            <label htmlFor={cell}>{cell}</label>
+                          </td>
+                        );
                       }
-                    />
-                    <input
-                      type="hidden"
-                      name={uniqueId}
-                      id={`lastInput-${uniqueId}`}
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                    })}
+                    <td>
+                      <input
+                        type="radio"
+                        form={uniqueId}
+                        name={uniqueId}
+                        value=""
+                        onClick={(e) =>
+                          (document.getElementById(
+                            `lastInput-${uniqueId}`
+                          ).type = "number")
+                        }
+                      />
+                      <input
+                        type="hidden"
+                        name={uniqueId}
+                        id={`lastInput-${uniqueId}`}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <section>
+            <p>Celkem hodin: {totalHours}</p>
+            <label htmlFor="wagePerHour">Hodinová sazba:</label>
+            <input type="number" name="wagePerHour" value={wagePerHour} onChange={(e) => setWagePerHour(e.target.value)} />
+            <p>Celková cena za projekt: {totalHours * wagePerHour}</p>
+          </section>
+        </>
       )}
     </div>
   );
